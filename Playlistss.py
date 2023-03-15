ac860bbc35680ff5ea6e4b49f6be5311412c896d
Programming Space <81325828+ProgrammingSpaces@users.noreply.github.com>
2023-03-16 01:13:59 +0200

    Add files via upload

---
+++ Playlistss.py
@@ -0,0 +1,94 @@
+from tkinter import *
+from tkinter import filedialog
+from pytube import Playlist
+import webbrowser
+
+# Url input And Printing Processes
+
+url_input=input("Put Your URL Here : ")
+print("\n")
+process_list=["Open", "Download"]
+print("--------------------------------------------------------------------")
+list_enum=1
+for i in process_list:
+    print(f"{list_enum} : {i}")
+    list_enum=list_enum+1
+print("--------------------------------------------------------------------")
+print("\n")
+
+# Choices
+
+process_input=int(input("Choose Process To Complete : "))
+
+# open in browser
+
+if process_input == 1:
+    webbrowser.open(url_input)
+
+# Download Functions
+
+elif process_input == 2:
+    print("\n")
+    print("""
+
+----------------------------------------------------------
+          All Videos Inside Current Playlist
+----------------------------------------------------------
+
+    """)
+    all_videos=Playlist(url_input).videos
+
+  # Print All Videos Titles
+    print("-------------------------------------------------------")
+    video_enum=1
+    for i in all_videos:
+        print(f"{video_enum} : {i.title}")
+        video_enum=video_enum+1
+    print("-------------------------------------------------------")
+    print("\n")
+
+    # Confirmation inputing
+
+    conf_input=input("Yes To Continue And No To Exit : ")
+    print("\n")
+    if conf_input.lower().startswith("y"):
+        forlder_name = filedialog.askdirectory()
+
+
+        # Printing Title And Streams OF Current Video
+
+
+        for i in all_videos:
+            print(str(f"The current video Name is : {i.title}"))
+
+            print("""
+
+------------------------------------------------------------            
+            Current Video Available Streams
+------------------------------------------------------------
+
+            """)
+            stm_enum=0
+            for n in i.streams.all():
+                print(f"{stm_enum} : {n}")
+                stm_enum=stm_enum+1
+
+            # Choosing Stream And Download The Choice
+            
+            stm_input=int(input("Please Choose Suitable Stream : "))
+
+            video=i.streams.all()[stm_input]
+            video.download(forlder_name)
+
+    # Cancellation Functions
+
+    elif conf_input.lower().startswith("n"):
+        print("OK, Good bye")
+        exit()
+    else:
+        print("Write A correct Answer :(")
+        exit()
+
+else:
+    print("\nChoose Right Choice :(")
+    exit()
